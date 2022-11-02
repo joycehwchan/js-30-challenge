9f7a4830e213496a923a55eccf21584d4338569f
Joyce Chan <joycehwchan@gmail.com>
2022-11-03 08:54:45 +0900

    refactor arrow functions

--- 03 - CSS Variables/script.js
+++ 03 - CSS Variables/script.js
@@ -1,12 +1,15 @@
-const inputs = document.querySelectorAll('.controls input')
+const inputs = document.querySelectorAll(".controls input");
 
-function handleUpdate() {
+const handleUpdate = (e) => {
   // console.log(this.value);
-  // console.log(this.dataset)
-  const suffix = this.dataset.sizing || ''; // => 'px' or ''
+  // console.log(this.dataset);
+  const suffix = e.currentTarget.dataset.sizing || ""; // => 'px' or ''
   // console.log(this.name) => `this` => `input`
-  document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix)
-}
+  document.documentElement.style.setProperty(
+    `--${e.currentTarget.name}`,
+    e.currentTarget.value + suffix
+  );
+};
 
-inputs.forEach(input => input.addEventListener('change', handleUpdate));
-inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+inputs.forEach((input) => input.addEventListener("change", handleUpdate));
+inputs.forEach((input) => input.addEventListener("mousemove", handleUpdate));
